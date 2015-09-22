51dce797de3d0949605d3a17f17db41220dc1a0a
Jonny Elliott <jonnynelliott@googlemail.com>
2015-09-22 16:14:05 -0400

    Added base files, travis and coverage

---
+++ tests/unittests/test_cscrape.py
@@ -0,0 +1,16 @@
+"""
+Unit tests placeholder
+"""
+from unittest import TestCase
+
+
+class TestConsulScrape(TestCase):
+    """
+    Unit tests for all of the main scrape methods
+    """
+
+    def test_pass(self):
+        """
+        Dummy test
+        """
+        self.assertEqual(1, 1)
