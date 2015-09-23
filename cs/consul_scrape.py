9584ebeed7c771ec3f3469247637bcefb1079a0a
Jonny Elliott <jonnynelliott@googlemail.com>
2015-09-23 16:59:07 -0400

    Basic scraping of consul
    Simple package to scrape and dump a specific set of consul variables.
    
    Relevant tests included. Consul response has been mocked using mock.
    AWS S3 responses have been mocked by moto.

---
+++ cs/consul_scrape.py
@@ -0,0 +1,62 @@
+"""
+Base file for consul scrape
+"""
+
+import json
+import boto3
+import consulate
+
+
+class ConsulScrape(object):
+    """
+    Class that contains the relevant methods and properties to scrape consul
+    content and dump to S3. More for tidiness than anything.
+    """
+
+    def __init__(self, service, environment='staging', **kwargs):
+        """
+        Constructor
+
+        :param service: service to be scraped from consul
+        """
+        self.config = {}
+        self.s3_bucket = 'etc-consul-scrape'
+        self.session = self.consul_session(**kwargs)
+        self.environment = environment
+        self.service = service if isinstance(service, list) else [service]
+        self.search_terms = [
+            'config/{service}/{environment}'.format(
+                service=service,
+                environment=self.environment
+            ) for service in self.service
+            ]
+
+    @staticmethod
+    def consul_session(**kwargs):
+        """
+        Start a consul session
+        :param kwargs: parameters to be passed to consulate
+        :return: session to consul
+        """
+        return consulate.Consul(**kwargs)
+
+    def get_config(self):
+        """
+        Retrieves relevant keys from the consul service
+        :return: list of dictionaries
+        """
+        [self.config.setdefault(service, self.session.kv.find(search_term))
+         for service, search_term in zip(self.service, self.search_terms)]
+        return self.config
+
+    def config_to_s3(self):
+        """
+        Sends the contents of config to s3 storage
+        """
+        s3_resource = boto3.resource('s3')
+        for service in self.service:
+            print json.dumps(self.config[service])
+            s3_resource.Bucket(self.s3_bucket).put_object(
+                Key='{service}.config.json'.format(service=service),
+                Body=json.dumps(self.config[service])
+            )
--- cs/tests/unittests/test_cscrape.py
+++ cs/tests/unittests/test_cscrape.py
@@ -1,7 +1,12 @@
 """
 Unit tests placeholder
 """
+from cs.consul_scrape import ConsulScrape
+from moto import mock_s3
 from unittest import TestCase
+
+import mock
+import boto3
 
 
 class TestConsulScrape(TestCase):
@@ -9,8 +14,88 @@ class TestConsulScrape(TestCase):
     Unit tests for all of the main scrape methods
     """
 
-    def test_pass(self):
+    def setUp(self):
         """
-        Dummy test
+        Generic setup of the tests
         """
-        self.assertEqual(1, 1)
+
+        self.service = 'adsws'
+        self.environment = 'staging'
+        self.stub_service_payload = {
+            'config/{}/{}/DEBUG'.format(self.service, self.environment): False,
+            'config/{}/{}/CLIENT'.format(self.service, self.environment): {
+                'TOKEN': 'we will provide an api key token for this application'
+            }
+        }
+
+    def test_functional_blueprint(self):
+        """
+        List of entire steps need to be carried out by the consul-scraper
+        """
+
+        # Scraper pulls all the key/values from consul
+
+        # Scraper dumps each of the key/values to S3 storage
+
+    @mock.patch('cs.consul_scrape.consulate.Consul')
+    def test_get_consul_keys_in_memory(self, mocked_class):
+        """
+        Tests that we can obtain the consul keys
+        """
+
+        # Mock the consulate session class
+        session = mocked_class.return_value
+        session.kv.find.return_value = self.stub_service_payload
+
+        cs = ConsulScrape(service=self.service, port=8500)
+        cs.get_config()
+
+        self.assertEqual(
+            cs.config[self.service].keys(),
+            self.stub_service_payload.keys(),
+            msg='Configuration keys do not match expected {} != {}'.format(
+                cs.config,
+                self.stub_service_payload
+            )
+        )
+        self.assertAlmostEqual(
+            cs.config[self.service].items(),
+            self.stub_service_payload.items(),
+            msg='Configuration items do not match expected {} != {}'.format(
+                cs.config,
+                self.stub_service_payload
+            )
+        )
+
+    @mock.patch('cs.consul_scrape.consulate.Consul')
+    @mock_s3
+    def test_upload_consul_keys_to_s3(self, mocked_class):
+        """
+        Test that the key/values obtained are uploaded to s3 storage
+        """
+
+        # Mock the consulate session class
+        session = mocked_class.return_value
+        session.kv.find.return_value = self.stub_service_payload
+
+        cs = ConsulScrape(service=self.service, port=8500)
+        cs.get_config()
+
+        # Setup the fake moto resources
+        s3_resource = boto3.resource('s3')
+        s3_resource.create_bucket(Bucket=cs.s3_bucket)
+
+        cs.config_to_s3()
+
+        # Check it got stored
+        s3_object = s3_resource.Object(
+            cs.s3_bucket,
+            '{service}.config.json'.format(service=self.service)
+        )
+
+        keys = s3_object.get().keys()
+
+        self.assertTrue(
+            len(keys) > 0
+        )
+
